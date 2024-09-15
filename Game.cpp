08a710a2f95e35bbb8c9f05c9ddc260434da6035
Xavier-Orrala <xavierorrala12404@gmail.com>
2024-09-15 11:14:59 -0400

    Added IntroMessage() function which welcomes the player to the game and gives the players the rules and reminders before the game begins.

--- Game.cpp
+++ Game.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
 using namespace std;
+void IntroMessage();
 
 int main() {
 
-	cout << "Tic Tac Toe goes here eventually..." << endl;
+	IntroMessage();
+}
 
+void IntroMessage()
+{
+	cout << "Welcome to Overflow's Tic Tac Toe game!\n";
+	cout << "Here are the rules/reminders:\n";
+	cout << "-------------------------------\n\n";
+	cout << "A Player must get 3 marks in a row to win. They can be vertical, horizontal, or diagonal.\n";
+	cout << "The Player who is 'x' will go first. The Player who is 'o' goes second.\n";
+	cout << "A win counter will be displayed at the end of each game.\n";
+	cout << "Resetting the board will not reset the win counter, restarting the program will however.\n\n";
 }
