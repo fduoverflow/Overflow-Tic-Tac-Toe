83e0bf7aba9808625da92b161c8b3cca5a12d0e6
tiffanylakecoelho <tiffanylakecoelho@gmail.com>
2024-09-15 22:45:38 -0400

    visualized the board to display (empty) at start of game. includes space guide

--- Board.h
+++ Board.h
@@ -1,3 +1,33 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 // This Header File will include the Board class
 // It will have the board states, check for illegal moves, reset the board, and check for victory conditions
+
+class Board {
+public:
+	Board();
+	void DisplayBoard();
+private:
+	char board[9];
+};
+
+Board::Board() {
+	for (char& space : board) {
+		space = ' ';
+	}
+}
+
+void Board::DisplayBoard() {
+	cout << "    A   B   C" << endl;
+	cout << "   ----------- " << endl;
+	for (int r = 0; r < 3; r++) {
+		cout << r + 1;
+		for (int c = 0; c < 3; c++) {
+			cout << " | " << board[3 * r + c];
+		}
+		cout << " |" << endl;
+	}
+}
